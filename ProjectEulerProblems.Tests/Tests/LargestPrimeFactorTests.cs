f3e83ecfdb6039b7e2ce09e3069732ed91d437d5
jjvdberg <jochemvandeberg@hotmail.com>
2020-06-12 11:28:57 +0200

    Solution to problem #3 Biggest Prime Factors

---
+++ ProjectEulerProblems.Tests/Tests/LargestPrimeFactorTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace ProjectEulerProblems.Tests.Tests {
+    public class LargestPrimeFactorTests {
+
+        [Theory]
+        [InlineData(29, 13195)]
+        [InlineData(6857, 600851475143)]
+        public void FindLargestPrimeFactor_ShouldWork(long expected, long number) {
+
+            long actual = LargestPrimeFactor.BiggestPrimeFactor(number);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ ProjectEulerProblems/Problems/LargestPrimeFactor.cs
@@ -0,0 +1,37 @@
+﻿public class LargestPrimeFactor {
+
+    public static bool IsPrime(long number) {
+
+        if (number < 2) {
+            return false;
+        }
+
+        for (long i = 2; i * i <= number; i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    /// <summary>
+    /// The prime factors of 13195 are 5, 7, 13 and 29.
+    /// What is the largest prime factor of the number 600851475143 ?
+    /// </summary>
+    /// <param name="number"></param>
+    /// <returns>The largest prime factor</returns>
+    public static long BiggestPrimeFactor(long number) {
+
+        for (long i = 2; i < number / 2; i++) {
+            var factor = number / i;
+            if (IsPrime(factor)) {
+                if (number % factor == 0) {
+                    return factor;
+                }
+            }
+        }
+
+        return -1;
+    }
+}
