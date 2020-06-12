27e78f7e2cda3302d895aeb2d5648f94ffa923df
jjvdberg <jochemvandeberg@hotmail.com>
2020-06-12 11:04:46 +0200

    Solved problem #2 Even Fibonacci numbers

---
+++ ProjectEulerProblems.Tests/Tests/EvenFibonacciNumbersTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace ProjectEulerProblems.Tests.Tests {
+    public class EvenFibonacciNumbersTests {
+
+        [Fact]
+        public void FindSumOfAllEvenFibonacciSequencesBelowMax_ShouldWork() {
+
+            int expected = 4613732;
+
+            int actual = EvenFibonacciNumbers.SumOfAllFibonacciEvenNumbers(4000000);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
