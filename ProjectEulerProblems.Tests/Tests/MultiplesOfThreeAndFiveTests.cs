2dd0c8c5b1264997cea2c4f6e6a4e23a4b507c07
jjvdberg <jochemvandeberg@hotmail.com>
2020-06-12 10:45:30 +0200

    solution to problem #1

---
+++ ProjectEulerProblems.Tests/Tests/MultiplesOfThreeAndFiveTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace ProjectEulerProblems.Tests.Tests {
+    public class MultiplesOfThreeAndFiveTests {
+
+        [Theory]
+        [InlineData(23, 9)]
+        [InlineData(233168, 999)]
+        public void FindSumOfAllMultiplesOfThreeOrFive_ShouldWork(int expected, int total) {
+
+            int actual = MultiplesOfThreeAndFive.SumOfAllMultiplesOfThreeOrFive(total);
+
+            Assert.Equal<int>(expected, actual);
+        }
+    }
+}
---
+++ ProjectEulerProblems/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace ProjectEulerProblems {
+    class Program {
+        static void Main() {
+            Console.WriteLine();
+        }
+    }
+}
