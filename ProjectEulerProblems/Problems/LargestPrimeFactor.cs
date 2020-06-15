443cabf19b308dfed625298e16dd4affa899115f
jjvdberg <jochemvandeberg@hotmail.com>
2020-06-15 12:39:41 +0200

    Some code cleanup

--- ProjectEulerProblems/Problems/LargestPrimeFactor.cs
+++ ProjectEulerProblems/Problems/LargestPrimeFactor.cs
@@ -1,37 +1,46 @@
-﻿public class LargestPrimeFactor {
-
-    public static bool IsPrime(long number) {
-
-        if (number < 2) {
-            return false;
-        }
-
-        for (long i = 2; i * i <= number; i++) {
-            if (number % i == 0) {
-                return false;
-            }
-        }
-
-        return true;
-    }
+﻿namespace ProjectEulerProblems {
 
     /// <summary>
+    /// Project Euler problem #3 
+    /// 
     /// The prime factors of 13195 are 5, 7, 13 and 29.
     /// What is the largest prime factor of the number 600851475143 ?
     /// </summary>
-    /// <param name="number"></param>
-    /// <returns>The largest prime factor</returns>
-    public static long BiggestPrimeFactor(long number) {
+    public class LargestPrimeFactor {
 
-        for (long i = 2; i < number / 2; i++) {
-            var factor = number / i;
-            if (IsPrime(factor)) {
-                if (number % factor == 0) {
-                    return factor;
+        public static bool IsPrime(long number) {
+
+            if (number < 2) {
+                return false;
+            }
+
+            for (long i = 2; i * i <= number; i++) {
+                if (number % i == 0) {
+                    return false;
                 }
             }
+
+            return true;
         }
 
-        return -1;
+        /// <summary>
+        /// The prime factors of 13195 are 5, 7, 13 and 29.
+        /// What is the largest prime factor of the number 600851475143 ?
+        /// </summary>
+        /// <param name="number"></param>
+        /// <returns>The largest prime factor</returns>
+        public static long BiggestPrimeFactor(long number) {
+
+            for (long i = 2; i < number / 2; i++) {
+                var factor = number / i;
+                if (IsPrime(factor)) {
+                    if (number % factor == 0) {
+                        return factor;
+                    }
+                }
+            }
+
+            return -1;
+        }
     }
 }
--- ProjectEulerProblems/Problems/MultiplesOfThreeAndFive.cs
+++ ProjectEulerProblems/Problems/MultiplesOfThreeAndFive.cs
@@ -1,32 +1,40 @@
-﻿using System;
-
-public class MultiplesOfThreeAndFive {
-
-    public static bool IsMultipleOfThree(int nr) {
-        return (nr % 3 == 0);
-    }
-
-    public static bool IsMultipleOfFive(int nr) {
-        return (nr % 5 == 0);
-    }
+﻿namespace ProjectEulerProblems {
 
     /// <summary>
+    /// Project Euler Problem #1
+    /// 
     /// If we list all the natural numbers below 10 that are multiples of 3 or 5, 
     /// we get 3, 5, 6 and 9.The sum of these multiples is 23.
     /// Find the sum of all the multiples of 3 or 5 below 1000.
     /// </summary>
-    /// <param name="total"></param>
-    /// <returns>int sum: The total sum of all multiples of 3 and 5</returns>
-    public static int SumOfAllMultiplesOfThreeOrFive(int total) {
+    public class MultiplesOfThreeAndFive {
 
-        int sum = 0;
-
-        for (int i = 0; i <= total; i++) {
-            if (IsMultipleOfThree(i) || IsMultipleOfFive(i)) {
-                sum += i;
-            }
+        public static bool IsMultipleOfThree(int nr) {
+            return (nr % 3 == 0);
         }
 
-        return sum;
+        public static bool IsMultipleOfFive(int nr) {
+            return (nr % 5 == 0);
+        }
+
+        /// <summary>
+        /// If we list all the natural numbers below 10 that are multiples of 3 or 5, 
+        /// we get 3, 5, 6 and 9.The sum of these multiples is 23.
+        /// Find the sum of all the multiples of 3 or 5 below 1000.
+        /// </summary>
+        /// <param name="total"></param>
+        /// <returns>int sum: The total sum of all multiples of 3 and 5</returns>
+        public static int SumOfAllMultiplesOfThreeOrFive(int total) {
+
+            int sum = 0;
+
+            for (int i = 0; i <= total; i++) {
+                if (IsMultipleOfThree(i) || IsMultipleOfFive(i)) {
+                    sum += i;
+                }
+            }
+
+            return sum;
+        }
     }
 }
