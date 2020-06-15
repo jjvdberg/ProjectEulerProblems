1c538b0d21ec248e0cbf5f87673bc354d83827a3
jjvdberg <jochemvandeberg@hotmail.com>
2020-06-15 12:22:49 +0200

    added problem #4, Largest Palindrone Product
    Including solution and tests for the solution

---
+++ ProjectEulerProblems.Tests/Tests/LargestPalindronePruductTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xunit;
+
+namespace ProjectEulerProblems.Tests.Tests {
+    public class LargestPalindronePruductTests {
+
+        [Theory]
+        [InlineData(2, 9009)]
+        [InlineData(3, 906609)]
+        public void FindLargestPalindronePruduct_ShouldWork(int NrOfDigits, int expected) {
+
+            int actual = LargestPalindronePruduct.CalculateLargestPalindroneProduct(NrOfDigits)[0];
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
